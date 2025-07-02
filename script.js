d68d1ce04081d247bed7de2055652fb33353df14
Kamilly <cordeiro.santos.kamilly@escola.pr.gov.br>
2025-07-02 13:37:32 +0000

    corrigindo erros

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavras.addEventListener('click,mostraPalavraChave');
+botaoMostraPalavras.addEventListener('click' , mostraPalavraChave);
 
 function mostraPalavraChave (){
-   const texto = document.querySelector('#entrada-de-texto')value;
-   const campoResultado = document.querySelector('#resultado-palavrachave');
-
-   campoResultado.textContent = texto;
+   alert('Fui clicado!');
 }
