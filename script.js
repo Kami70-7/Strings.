857c78ee138f28b379d7889c15d7f7a306de70c3
Kamilly <cordeiro.santos.kamilly@escola.pr.gov.br>
2025-07-02 21:02:17 +0000

    tentando corrigir erros

--- script.js
+++ script.js
@@ -13,18 +13,17 @@ function mostraPalavraChave (){
 
 function processaTexto(texto){
   let palavras = texto.split(/\P{L}+/u);
+  contaFrequencias(palavras);
+  return palavras;
+}
 
+function contaFrequencias(palavras){
   let frequencias = {};
   for(let i of palavras){
     frequencias[i] = 0;
-    for(let j of palavras){
-      if [i == j]{
-        frequencias[i]++;
-      }
+    for (let j of palavras){
+      if (i == j){
+
     }
   }
-
-  console.log(frequencias);
-
-  return palavras
 }
